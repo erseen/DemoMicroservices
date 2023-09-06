5c93072e0ff549d88584575f9ff9a357ecf561a8
ersen06 <ersenjok72@gmail.com>
2023-09-06 23:06:58 +0300

    customer api is done

---
+++ CustomerApi/Controllers/CustomerController.cs
@@ -0,0 +1,61 @@
+﻿using CustomerApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CustomerDbContext _customerDbContext;
+
+        public CustomerController(CustomerDbContext customerDbContext)
+        {
+            _customerDbContext = customerDbContext;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> GetCustomers() 
+        { 
+        return _customerDbContext.Customers;
+        
+        }
+        [HttpGet("{customerId:int}")]
+        public async Task<ActionResult<Customer>> GetById(int customerId)
+        {
+            var customer = await _customerDbContext.Customers.FindAsync(customerId);    
+            return customer;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Customer customer)
+        {
+            await _customerDbContext.Customers.AddAsync(customer);
+            await _customerDbContext.SaveChangesAsync();
+            return Ok();    
+        }
+        [HttpPut]
+        public async Task<ActionResult> Update(Customer customer)
+        {
+            if (customer == null) 
+            {
+                return NotFound(); 
+            }
+            _customerDbContext.Customers.Update(customer);
+            _customerDbContext.SaveChanges();   
+            return Ok();
+        }
+        [HttpDelete("{customerId:int}")]
+        public async Task<ActionResult> Delete(int customerId)
+        {
+             var customer =await _customerDbContext.Customers.FindAsync(customerId);
+            if(customer==null)
+            {
+                return NotFound();
+            }
+            _customerDbContext.Remove(customer);
+            await _customerDbContext.SaveChangesAsync();
+            return Ok();
+
+        }
+    }
+}
---
+++ CustomerApi/CustomerDbContext.cs
@@ -0,0 +1,34 @@
+﻿using CustomerApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace CustomerApi
+{
+    public class CustomerDbContext:DbContext
+    {
+        public CustomerDbContext(DbContextOptions<CustomerDbContext> dbContextOptions):base(dbContextOptions) 
+        {
+            try
+            {
+                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if(databaseCreator != null)
+                {
+                    if (!databaseCreator.CanConnect()) databaseCreator.Create();
+                    if(!databaseCreator.HasTables()) databaseCreator.CreateTables();
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message, "Database Connection Problemmmm !!!!!!!!");
+            }
+            
+        }
+        public DbSet <Customer> Customers { get; set; }
+
+    }
+}

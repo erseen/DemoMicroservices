ef9940c1c8aa7ae814a4645765d4d5e74f39cad4
ersen06 <ersenjok72@gmail.com>
2023-09-07 22:41:15 +0300

    Order api is done

---
+++ OrderApi/Controllers/OrderController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using OrderApi.Models;
+
+namespace OrderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMongoCollection<Order> _orderCollection;
+
+        public OrderController()
+        {
+            var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+            var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+            var connectionString = $"mongodb://{dbHost}:27017/{dbName}";
+
+
+            var mongoUrl = MongoUrl.Create(connectionString);
+            MongoClient mongoClient = new MongoClient(mongoUrl);
+            var database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+            _orderCollection = database.GetCollection<Order>("order");
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            return await _orderCollection.Find(Builders<Order>.Filter.Empty).ToListAsync();
+        }
+        [HttpGet("orderId")]
+        public async Task<ActionResult<Order>> GetById(string orderId)
+        {
+            var filterDefination = Builders<Order>.Filter.Eq(x=>x.OrderId, orderId); 
+            if (filterDefination == null)
+            {
+                return NotFound();
+            }
+            return await _orderCollection.Find(filterDefination).SingleOrDefaultAsync();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(Order order)
+        {
+            await _orderCollection.InsertOneAsync(order);
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<ActionResult> Update(Order order)
+        {
+            var filterDefination=Builders<Order>.Filter.Eq(x=>x.OrderId, order.OrderId);
+            if (filterDefination == null)
+            {
+                return NotFound();
+            }
+            await _orderCollection.ReplaceOneAsync(filterDefination, order);
+            return Ok();    
+        }
+        [HttpDelete("{orderId}")]
+        public async Task<ActionResult> Delete(string orderId)
+        {
+            var filterDefination=Builders<Order>.Filter.Eq(x=> x.OrderId, orderId);
+            if (filterDefination == null) 
+            { 
+            return NotFound();
+            }
+            await _orderCollection.DeleteOneAsync(filterDefination);
+            return Ok();    
+        }
+
+    }
+}
---
+++ OrderApi/Models/OrderedDetail.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using ThirdParty.BouncyCastle.Utilities.IO.Pem;
+
+namespace OrderApi.Models
+{
+    [Serializable, BsonIgnoreExtraElements]
+    public class OrderedDetail
+    {
+        [BsonId, BsonElement("product_id"), BsonRepresentation(BsonType.Int32)]
+        public int ProductId { get; set; }
+        [ BsonElement("quantity"), BsonRepresentation(BsonType.Decimal128)]
+        public decimal Quantity { get; set; }
+        [BsonElement("unit_price"), BsonRepresentation(BsonType.Decimal128)]
+        public decimal UnitPrice { get; set; }
+
+    }
+}
